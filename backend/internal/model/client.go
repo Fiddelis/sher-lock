6cb2368bc7257ab5d784223c3f22e271fb681727
Fiddelis <lucas.ruan@ges.inatel.br>
2024-07-25 21:36:49 -0300

    refactor: remove backend folder

--- backend/internal/model/client.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Client struct {
-	ID          int    `json:"id"`
-	Address     string `json:"address"`
-	Name        string `json:"name"`
-	PhoneNumber string `json:"phone_number"`
-	Mail        string `json:"mail"`
-}
--- backend/internal/model/drawer.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Drawer struct {
-	ID        int    `json:"id"`
-	LockerID  int    `json:"locker_id"`
-	Dimension string `json:"dimension"`
-	Available bool   `json:"available"`
-}
