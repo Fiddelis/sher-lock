5e3252cc9aa84ea50e93035b862eb86356a328eb
Fiddelis <lucas.ruan@ges.inatel.br>
2024-08-11 18:14:13 -0300

    feat: update html in mail

--- internal/messaging/mail.go
+++ internal/messaging/mail.go
@@ -12,8 +12,8 @@ import (
 
 func SendMailToClient(mail string, product model.Product, qrCode []byte) error {
 	// Configurações do servidor SMTP da Hostinger
-	smtpHost := ""
-	smtpPort := 1
+	smtpHost := "smtp.hostinger.com"
+	smtpPort := 587
 	smtpUser := ""
 	smtpPass := ""
 
@@ -21,22 +21,52 @@ func SendMailToClient(mail string, product model.Product, qrCode []byte) error {
 
 	// Configurações do email
 	to := mail
-	subject := "Sherlock - QRCode"
+	subject := "Sherlock - Você ja pode retirar sua compra com seu código QR"
 	body := `
-    <html>
-		<body>
-			<h1>QRCode Gerado para retirada</h1>
-			
-			<h3>Informações do Produto</h3>
-			<p><b>Nome:</b> ` + product.Name + `</p>
-			<p><b>Quantidade:</b> ` + strconv.FormatFloat(product.Quantity, 'f', -1, 64) + `</p>
-			<p><b>Data estimada:</b> ` + product.EstimatedDate + `</p>
-			<p><b>Local do locker para retirada:</b> ` + product.Address + `</p>
-			<h3>QR-Code para retirada</h3>
-			<img src="cid:qrcode.png">
-		</body>
-    </html>
-	`
+    <!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				background-color: #f4f4f4;
+			}
+	
+			.email-container {
+				max-width: 600px;
+				margin: 0 auto;
+				padding: 20px;
+				background-color: #ffffff;
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+    	<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" height="100%">
+        	<tr>
+            	<td align="center" valign="top">
+                	<div class="email-container">
+                    	<h1>QRCode Gerado para retirada</h1>
+                    	<h3>Informações do Produto</h3>
+                    	<p><b>Nome:</b> ` + product.Name + `</p>
+                    	<p><b>Quantidade:</b> ` + strconv.FormatFloat(product.Quantity, 'f', -1, 64) + `</p>
+                    	<p><b>Data estimada:</b> ` + product.EstimatedDate + `</p>
+                    	<p><b>Local para retirada:</b> ` + product.Address + `</p>
+                    	<div class="qrcode">
+                        	<h3>QR-Code para retirada</h3>
+                        	<img src="cid:qrcode.png">
+                    	</div>
+                	</div>
+            	</td>
+        	</tr>
+    	</table>
+	</body>
+	</html>
+`
 
 	m := gomail.NewMessage()
 	m.SetHeader("From", smtpUser)
