5ffa088389a26574b51d1310e15e8843c89ae821
Fiddelis <lucas.ruan@ges.inatel.br>
2024-09-09 17:57:38 -0300

    feat: dto to string

--- mail-service/src/main/java/com/sherlock/mailservice/model/RegisterDTO.java
+++ mail-service/src/main/java/com/sherlock/mailservice/model/RegisterDTO.java
@@ -2,9 +2,11 @@ package com.sherlock.mailservice.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class RegisterDTO {
     Client client;
     Product product;
